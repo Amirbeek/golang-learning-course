fb568d336bc6e44ecf03507c3c173a296f6892c7
amirbeek <Amirbek.shomurodov01@gmail.com>
2025-10-24 12:25:57 +0500

    updated

---
+++ backend-engineering/lesson-4 Posts CRUD/CRUD 8 - Updating and Deleting Posts/internal/store/posts.go
@@ -0,0 +1,67 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type Post struct {
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserId    int64     `json:"userId"`
+	Tags      []string  `json:"tags"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Comments  []*Comment  `json:"comments"`
+}
+
+type PostsStore struct {
+	db *sql.DB
+}
+
+func (s *PostsStore) Create(ctx context.Context, p *Post) error {
+	query := `
+		INSERT INTO posts (content, title, user_id, tags)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, updated_at
+	`
+
+	return s.db.QueryRowContext(
+		ctx,
+		query,
+		p.Content,
+		p.Title,
+		p.UserId,
+		pq.Array(p.Tags),
+	).Scan(&p.ID, &p.CreatedAt, &p.UpdatedAt)
+}
+
+func (s *PostsStore) GetById(ctx context.Context, id int64) (*Post, error) {
+	query := `
+SELECT id, content, title, user_id, tags, created_at, updated_at
+FROM posts WHERE id = $1`
+	var post Post
+	err := s.db.QueryRowContext(ctx, query, id).Scan(
+		&post.ID,
+		&post.Content,
+		&post.Title,
+		&post.UserId,
+		pq.Array(&post.Tags),
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &post, nil
+}
---
+++ backend-engineering/lesson-4 Posts CRUD/CRUD 8 - Updating and Deleting Posts/internal/store/storage.go
@@ -0,0 +1,32 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrNotFound = errors.New("record not found")
+)
+
+type Storage struct {
+	Posts interface {
+		Create(context.Context, *Post) error
+		GetById(context.Context, int64) (*Post, error)
+	}
+	Users interface {
+		Create(context.Context, *User) error
+	}
+	Comments interface {
+		GetByPostId(context.Context, int64) ([]*Comment, error)
+	}
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Posts:    &PostsStore{db},
+		Users:    &UserStore{db},
+		Comments: &CommentsStore{db},
+	}
+}
